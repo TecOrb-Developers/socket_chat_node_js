b4235480426c2c570b66fda9a69c16d3c7c44d55
Punit Sharma <punit@tecorb.co>
2022-11-08 18:16:14 +0530

    manage username through socket

--- app.js
+++ app.js
@@ -1,27 +1,23 @@
 const express = require('express');
 const app = express();
 
-
 //set the template engine ejs
 app.set('view engine', 'ejs');
 
 //middlewares
 app.use(express.static('public'));
 
-
 //routes
 app.get('/', (req, res) => {
     res.render('index');
-})
+});
 
 //Listen on port 3000
-server = app.listen(3000)
-
+server = app.listen(3000);
 console.log("Socket Server is running on port 3000");
 
 //socket.io instantiation
 const io = require("socket.io")(server);
-
 
 //listen on every connection
 io.on('connection', (socket) => {
@@ -43,6 +39,6 @@ io.on('connection', (socket) => {
 
     //listen on typing
     socket.on('typing', (data) => {
-        socket.broadcast.emit('typing', { username: socket.username })
+        socket.broadcast.emit('typing', { username: socket.username });
     });
-})
+});
--- public/chat.js
+++ public/chat.js
@@ -1,8 +1,7 @@
-
 $(function(){
    	//make connection
 	var socket = io.connect('http://localhost:3000');
-
+	
 	//buttons and inputs
 	var message = $("#message");
 	var username = $("#username");
@@ -14,29 +13,27 @@ $(function(){
 	//Emit message
 	send_message.click(function(){
 		socket.emit('new_message', {message : message.val()});
-	})
+	});
 
 	//Listen on new_message
 	socket.on("new_message", (data) => {
 		feedback.html('');
 		message.val('');
-		chatroom.append("<p class='message'>" + data.username + ": " + data.message + "</p>")
-	})
+		chatroom.append("<p class='message'>" + data.username + ": " + data.message + "</p>");
+	});
 
 	//Emit a username
 	send_username.click(function(){
 		socket.emit('change_username', {username : username.val()});
-	})
+	});
 
 	//Emit typing
 	message.bind("keypress", () => {
 		socket.emit('typing');
-	})
+	});
 
 	//Listen on typing
 	socket.on('typing', (data) => {
 		feedback.html("<p><i>" + data.username + " is typing a message..." + "</i></p>");
-	})
+	});
 });
-
-
